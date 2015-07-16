a69600a2ece6603133bfc69c947d9bd4e4a2b344
Chuck Yang <yangchuck@gmail.com>
2015-07-16 03:39:34 +0000

    display data in info window

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
+$(document).ready(function() {
+
+var dataset = null;
+function loadData() {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else {// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      dataset = JSON.parse(xmlhttp.responseText);
+    }
+  };
+  xmlhttp.open("GET","population_1040712.json",true);
+  xmlhttp.send();
+}
+
+function genInfoWindowContent(dataName) {
+  if (!dataset) return;
+  if (dataset.data.body.districts.indexOf(dataName) < 0) return;
+
+  var out = '<b>' + dataName + '</b>';
+  var stats = dataset.data.body.stats;
+  for (var cat in stats) {
+    var r = stats[cat];
+    out += '<ul>'+cat;
+    for (var prop in r[dataName]) {
+      var v = r[dataName][prop];
+      out += '<li>' + prop + ': ' + v + '</li>';
+    }
+    out += '</ul>';
+  }
+
+  return out;
+}
+
 var map;
 function initialize() {
+  loadData();
+
   // Create a simple map.
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 12,
-    center: {lat: 25.08, lng: 121.55}
+    center: { lat: 25.08, lng: 121.55 },
+    disableDefaultUI: true
   });
   
   var xmlhttp;
@@ -20,6 +61,7 @@ function initialize() {
   xmlhttp.open("GET","taipei.kml",true);
   xmlhttp.send();
 
+  var lastInfoWindowOpened = null;
   function onKMLDocFetched(kml) {
 	  var geoJson = toGeoJSON.kml(kml);
 	  // Load a GeoJSON from the same server as our demo.
@@ -35,25 +77,45 @@ function initialize() {
       });
 	  })
 	  
+	  var clickedFeature = null;
+	  map.data.addListener('click', function(event) {
+	    clickedFeature = event.feature;
+      map.data.overrideStyle(clickedFeature, { fillColor: 'red', strokeColor: 'red', strokeWeight: 3});
+	    var dataName = event.feature.getProperty('name');
+	    var contentString = genInfoWindowContent(dataName);
+	    if (contentString) {
+        var infoWindow = new google.maps.InfoWindow({ 
+          content: contentString
+        });
+        infoWindow.setPosition(event.latLng);
+        if (lastInfoWindowOpened) {
+          lastInfoWindowOpened.close();
+        }
+        lastInfoWindowOpened = infoWindow;
+        infoWindow.open(map);
+      }
+    });
+
 	  // When the user hovers, tempt them to click by outlining the letters.
 	  // Call revertStyle() to remove all overrides. This will use the style rules
 	  // defined in the function passed to setStyle()
 	  map.data.addListener('mouseover', function(event) {
       map.data.revertStyle();
-      map.data.overrideStyle(event.feature, { fillColor: 'red', strokeColor: 'red', strokeWeight: 3});
+      map.data.overrideStyle(event.feature, { strokeWeight: 3});
+      map.data.overrideStyle(clickedFeature, { fillColor: 'red', strokeColor: 'red', strokeWeight: 3});
       
       var district = event.feature.getProperty('name');
-      var elem = document.getElementById('district-text');
-      if (elem) {
-        elem.textContent = district;
-      }
+      $('.pageTitle').text(district);
 	  });
 
 	  map.data.addListener('mouseout', function(event) {
       map.data.revertStyle();
+      map.data.overrideStyle(clickedFeature, { fillColor: 'red', strokeColor: 'red', strokeWeight: 3});
 	  });
   };
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+$(".button-collapse").sideNav();
+});
---
+++ xlsToJson.js
@@ -0,0 +1,65 @@
+var xls = require('xlsjs');
+var fs = require('fs');
+
+if (process.argv.length != 4) {
+  console.log('Usage: node xlsToJson.js [inFile] [outFile]');
+  process.exit(-1);
+}
+
+var inFile = process.argv[2];
+var outFile = process.argv[3];
+var wb = xls.readFile(inFile);
+
+var out = {
+  lastModified: new Date().toJSON(),
+  license: 'CC0',
+  source: '臺北市資訊局',
+  data: {
+    metadata: {
+      title: '各區人口統計',
+      notes: '備註\n'+
+        '1.公民數為20歲以上人口。\n'+
+        '2.扶養比指每一百位有生產能力的成年人(15～64歲)所扶養或負擔無生產能力(14歲以下及65歲以上)的人口比例。\n'+
+        '3.新移民東南亞前5國為越南、印尼、泰國、菲律賓、緬甸。\n'+
+        '4.本表統計期間截至104年5月止。'
+    },
+    body: {
+      districts: [],
+      categories: wb.SheetNames,
+      stats: {}
+    }
+  }
+};
+
+
+for (var sn in wb.Sheets) {
+  var ws = wb.Sheets[sn];
+  var wsJson = xls.utils.sheet_to_json(ws, { raw: true });
+  var districts = [];
+
+  for (var i = 0; i < wsJson.length; ++i) {
+    var r = wsJson[i];
+
+    var d = r['行政區'];
+    districts.push(d);
+
+    if (!out.data.body.stats[sn]) {
+      out.data.body.stats[sn] = {};
+    }
+    if (!out.data.body.stats[sn][d]) {
+      out.data.body.stats[sn][d] = {};
+    }
+    
+    for (var prop in r) {
+      if (prop == '行政區') continue;
+
+      out.data.body.stats[sn][d][prop] = r[prop];
+    }
+  }
+
+  if (out.data.body.districts.length == 0) {
+    out.data.body.districts = districts;
+  }
+}
+
+fs.writeFileSync(outFile, JSON.stringify(out, undefined, 2));
